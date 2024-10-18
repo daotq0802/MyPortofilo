7e45c6c78827e7e4217be2a6683150ad0ac09993
Quý Đạo <tq.dao0802@gmail.com>
2024-10-19 00:14:13 +0900

    header section update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,75 @@
 function Header() {
     return (
         <>
-            <header className="flex justify-around items-center h-20 fixed w-full max-xl:gap-0 max-lg:gap-1 max-md:gap-2 max-sm:gap-3" >
-                <img src="src/assets/MyLogo.png" alt="My Logo" className="max-sm:w-10 max-md:w-20"/>
-                <nav className="">
-                    <a href="#" className="">Home</a>
-                    <a href="#" className="">About</a>
-                    <a href="#" className="">Project</a>
-                    <a href="#" className="">Contact</a>
+            <header
+                className="w-full flex fixed 
+                                        md:justify-evenly md:shadow-sm md:shadow-slate-400
+                                        max-md:justify-around items-center h-20 py-2"
+            >
+                <img
+                    src="src/assets/MyLogo.png"
+                    alt="My Logo"
+                    className="max-sm:w-16 max-md:w-20 md:w-1/12 h-full cursor-pointer"
+                />
+                <nav
+                    className="w-2/3  px-2 py-2 
+                                        md:flex md:gap-10 md:justify-evenly
+                                        max-md:animate-slideDown max-md:absolute max-md:bottom-0 max-md:flex-col max-md:border-t-2 max-md:gap-2 max-md:items-start"
+                >
+                    <a
+                        href="#"
+                        className="font-bold transition duration-500 py-2
+                        max-lg:text-base
+                        lg:text-xl
+                        md:self-center"
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="#"
+                        className="font-bold transition duration-500 py-2
+                        max-lg:text-base
+                        lg:text-xl 
+                        md:self-center"
+                    >
+                        About
+                    </a>
+                    <a
+                        href="#"
+                        className="font-bold transition duration-500 py-2
+                        max-lg:text-base
+                        lg:text-xl 
+                        md:self-center"
+                    >
+                        Project
+                    </a>
+                    <a
+                        href="#"
+                        className="font-bold transition duration-500 py-2 
+                        max-lg:text-base 
+                        lg:text-xl 
+                        md:self-center"
+                    >
+                        Contact
+                    </a>
+                    <button
+                        className="px-5 py-2 rounded-2xl font-bold bg-[#9d7979] transition duration-500 
+                        max-lg:text-base
+                        lg:text-xl lg:hover:bg-[#4ec0f4] lg:hover:text-white
+                        md:self-center"
+                    >
+                        Download CV
+                    </button>
                 </nav>
-                <button className="">Download CV</button>
+                <i
+                    onClick={showMenu}
+                    className="fa-solid fa-bars md:hidden max-md:inline-block text-3xl p-2.5"
+                ></i>
             </header>
         </>
     );
 }
-
+function showMenu() {
+    document.getElementsByTagName("nav")[0].classList.toggle("active");
+}
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from "typescript";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,9 +8,26 @@ export default {
             sm: "480px",
             md: "768px",
             lg: "1024px",
-            xl: "1280px",
+            xl: "1440px",
         },
-        extend: {},
+        extend: {
+            keyframes: {
+                slideDown: {
+                    "0%": {
+                        opacity: 0,
+                        transform: "translateY(80%)",
+                    },
+                    "100%": {
+                        opacity: 1,
+
+                        transform: "translateY(100%)",
+                    },
+                },
+            },
+            animation: {
+                slideDown: "slideDown 1s ease forwards",
+            },
+        },
     },
     plugins: [],
 };
