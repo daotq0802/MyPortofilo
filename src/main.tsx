6e42afa3d7bb1ccc0650a83f8f9b80f42ac08bbe
Quý Đạo <tq.dao0802@gmail.com>
2024-10-20 15:54:06 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,17 @@ import Project from "./pages/Project.tsx";
 import Contact from "./pages/Contact.tsx";
 import About from "./pages/About.tsx";
 // import ErrorPage from "./pages/ErrorPage.tsx";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
             <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/project" element={<Project />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/MyPortofilo" element={<Index />} />
+                <Route path="/MyPortofilo/home" element={<Home />} />
+                <Route path="/MyPortofilo/about" element={<About />} />
+                <Route path="/MyPortofilo/project" element={<Project />} />
+                <Route path="/MyPortofilo/contact" element={<Contact />} />
             </Routes>
         </Router>
     </StrictMode>
