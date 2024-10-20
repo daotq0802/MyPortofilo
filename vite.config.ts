636d9de41d27ce19f39c38d13cb3e3df512f256b
Quý Đạo <tq.dao0802@gmail.com>
2024-10-20 15:18:16 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: "https://daotq0802.github.io/MyPortofilo/",
+    base: "https://daotq0802.github.io/MyPortofilo",
 });
